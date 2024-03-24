311162e1512775c70348c3591b0f77d71adfeaed
Nikhil Venkat <64997334+Nike-Hill@users.noreply.github.com>
2024-03-24 16:03:56 -0700

    Fixed expo-router dependecy conflict, but now there's new runtime errors. I think it's something with babel config.

--- LinkedIn-Clone/src/app/(tabs)/new-post.tsx
+++ LinkedIn-Clone/src/app/(tabs)/new-post.tsx
@@ -2,12 +2,17 @@ import { Pressable, StyleSheet, TextInput } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { useNavigation, useRouter } from 'expo-router';
 import { useLayoutEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { FontAwesome } from '@expo/vector-icons'
 
 export default function NewPostScreen() {
   const[content, setContent] = useState('');
+  const [image, setImage] = useState<string | null>(null);
+
 
   const navigation = useNavigation();
   const router = useRouter();
+
 
   const onPost = () => {
     console.warn(`Posting: ${content}`);
@@ -26,6 +31,22 @@ export default function NewPostScreen() {
     });
   }, [onPost]);
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      // aspect: [4, 3],
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput 
@@ -35,6 +56,10 @@ export default function NewPostScreen() {
       style={styles.input}
       multiline
       />
+
+      <View style={styles.footer}>
+        <FontAwesome name='image' size={24} color='gray' style={styles.iconButton} onPress={pickImage}/>
+      </View>
     </View>
   );
 }
@@ -66,5 +91,13 @@ const styles = StyleSheet.create({
   postButtonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+
+  footer: {
+    marginTop: 'auto',
+  },
+
+  iconButton: {
+    backgroundColor: 'gainsboro'
   }
 });
